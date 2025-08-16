220d17b75cfbb724fd1554ab963896e5b86710f0
jw0785 <143842176+jw0785@users.noreply.github.com>
2025-08-15 22:43:55 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,30 @@
+# Fire Chrome
+Make Firefox look proper
+## Find profile folder
+under `about:support` click **Open Folder** in row **profile folder**
+## Drop files
+- drop `user.js` in this dir
+- create **chrome** dir if it doesn't exist, drop both css into **chrome**
+## Before and After
+<table>
+    <tr>
+        <td width="50%">
+            <p align="center"><strong>Default Firefox</strong></p>
+            <img src="doc/before.png" alt="Default Firefox">
+        </td>
+        <td width="50%">
+            <p align="center"><strong>Fire Chrome</strong></p>
+            <img src="doc/after.png" alt="Fire Chrome">
+        </td>
+    </tr>
+    <tr>
+        <td width="30%">
+            <p align="center"><strong>Default Firefox</strong></p>
+            <img src="doc/default_context.png" alt="Default Firefox">
+        </td>
+        <td width="70%">
+            <p align="center"><strong>Fire Chrome</strong></p>
+            <img src="doc/fixed_context.png" alt="Fire Chrome">
+        </td>
+    </tr>
+</table>
---
+++ user.js
@@ -0,0 +1,64 @@
+// ui & ux
+user_pref("browser.aboutConfig.showWarning", false);
+user_pref("browser.bookmarks.showMobileBookmarks", false);
+user_pref("browser.toolbars.bookmarks.visibility", "never");
+user_pref("browser.tabs.tabMinWidth", 90);
+user_pref("browser.startup.page", 3);
+user_pref("browser.engagement.home-button.has-removed", true);
+user_pref("browser.newtabpage.activity-stream.feeds.section.topstories", false);
+user_pref("browser.newtabpage.activity-stream.feeds.topsites", false);
+user_pref("findbar.highlightAll", true);
+user_pref("full-screen-api.transition-duration.enter", "0 0");
+user_pref("full-screen-api.transition-duration.leave", "0 0");
+user_pref("full-screen-api.warning.timeout", 0);
+user_pref("mousewheel.min_line_scroll_amount", 6);
+user_pref("toolkit.legacyUserProfileCustomizations.stylesheets", true);
+// Font
+user_pref("font.default.x-western", "sans-serif");
+user_pref("font.name.monospace.x-western", "JetBrains Mono");
+user_pref("font.name.sans-serif.x-western", "SF Pro Display");
+user_pref("font.name.serif.x-western", "Times New Roman");
+// Touch
+user_pref("apz.allow_double_tap_zooming", false);
+user_pref("apz.overscroll.enabled", false);
+user_pref("apz.touch_start_tolerance", "0.2");
+user_pref("browser.touchmode.auto", false);
+user_pref("ui.click_hold_context_menus.delay", 175);
+// Hardware acceleration
+user_pref("media.hardware-video-decoding.failed", false);
+// Color management
+user_pref("gfx.color_management.mode", 1);
+user_pref("gfx.webrender.all", true);
+// Lazy loading workaround
+user_pref("dom.image-lazy-loading.root-margin.bottom", "10000");
+user_pref("dom.image-lazy-loading.root-margin.left", "10000");
+user_pref("dom.image-lazy-loading.root-margin.right", "10000");
+user_pref("dom.image-lazy-loading.root-margin.top", "10000");
+// Force PiP
+user_pref("media.videocontrols.picture-in-picture.respect-disablePictureInPicture", false);
+//Privacy
+user_pref("accessibility.force_disabled", 1);
+user_pref("app.shield.optoutstudies.enabled", false);
+user_pref("browser.discovery.enabled", false);
+user_pref("browser.display.document_color_use", 1);
+user_pref("browser.search.suggest.enabled.private", true);
+user_pref("browser.urlbar.suggest.bookmark", false);
+user_pref("browser.urlbar.suggest.engines", false);
+user_pref("browser.urlbar.suggest.history", false);
+user_pref("browser.urlbar.suggest.quicksuggest.nonsponsored", false);
+user_pref("browser.urlbar.suggest.quicksuggest.sponsored", false);
+user_pref("browser.urlbar.suggest.recentsearches", false);
+user_pref("browser.urlbar.suggest.topsites", false);
+user_pref("browser.urlbar.suggest.trending", false);
+user_pref("datareporting.healthreport.uploadEnabled", false);
+user_pref("datareporting.usage.uploadEnabled", false);
+user_pref("dom.disable_open_during_load", false);
+user_pref("dom.private-attribution.submission.enabled", false);
+user_pref("extensions.openPopupWithoutUserGesture.enabled", true);
+user_pref("network.dns.disablePrefetch", true);
+user_pref("network.http.max-connections", 1500);
+user_pref("network.http.max-persistent-connections-per-server", 10);
+user_pref("network.http.speculative-parallel-limit", 0);
+user_pref("network.predictor.enabled", false);
+user_pref("network.prefetch-next", false);
+user_pref("signon.rememberSignons", false);
